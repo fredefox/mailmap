e7ea3c7576d2ef9429ad84af8c560a173fc05322
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-10-03 23:08:23 +0200

    Improve generator

--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,40 @@
+{-# language OverloadedStrings #-}
 module Main (main) where
 
 import Test.QuickCheck
 import qualified Mailmap
 import Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import Text.Printf
+import Control.Monad
+import Control.Applicative
 
 idempotent :: Eq a => (a -> a) -> a -> Bool
 idempotent f x = f (f x) == f x
 
 main :: IO ()
-main = quickCheck $ forAll g $ idempotent Mailmap.normalise
-  where
-  g :: Gen Text
-  g = Text.pack <$> arbitrary
+main = quickCheck $ forAll gLines $ idempotent Mailmap.normalise
+
+email :: Gen Text
+email = do
+  n <- chooseInt (0, 6)
+  pure $ Text.pack $ printf "<example+%d@example.org>" n
+
+name :: Gen Text
+name = do
+  n <- chooseInt (0, 6)
+  pure $ Text.pack $ printf "person-%d" n
+
+line :: Gen Text
+line = do
+  n <- name
+  k <- chooseInt (1, 4)
+  es <- replicateM k email
+  pure $ Text.unwords $ n:es
+  
+gLines :: Gen Text
+gLines = do
+  k <- chooseInt (0, 40)
+  ls <- replicateM k line
+  pure $ Text.unlines ls
